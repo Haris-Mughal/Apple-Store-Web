09d393cba106f5d846d5c7f1ebdae44b66aacf5f
onlytoseef <hellotoseef@gmail.com>
2025-01-24 22:09:52 +0500

    added new packages

--- src/pages/Frontend/Home/Home.tsx
+++ src/pages/Frontend/Home/Home.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <button className="bg-green-400  rounded-lg p-4  motion-safe:animate-bounce">
+        click on me
+      </button>
+    </>
+  );
 }
