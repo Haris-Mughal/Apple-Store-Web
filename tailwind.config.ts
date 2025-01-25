5c27b3229b4d7d8fd11b47797caaed15a64c3076
Haris Mughal <harismirza3456@gmail.com>
2025-01-25 23:20:39 +0500

    Font family added - Cabrion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        cabrion: ['Cabrion', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
