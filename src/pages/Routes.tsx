88047a1b34fc9ba47efee5767603e73ff7176c43
Haris Mughal <harismirza3456@gmail.com>
2025-01-25 23:22:31 +0500

    Auth routing done

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import Frontend from "./Frontend";
+import Auth from "./Auth";
 
 export default function Index() {
   return (
     <Routes>
       <Route path="/*" element={<Frontend />} />
+      <Route path="/auth/*" element={<Auth />} />
     </Routes>
   );
 }
